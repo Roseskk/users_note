eeb13f5767ce25ebbdf1238d34be9be312a44563
Roseskk <oleg419583@yandex.ru>
2023-04-03 11:48:48 +0300

    table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,52 @@
 import api from '../api'
+import {useState} from "react";
 
 const Users = () => {
-    console.log(api.users.fetchAll())
+    const [users, setUsers] = useState(api.users.fetchAll())
+    console.log(users)
+
+    // Не понял а с этим что сделать нужно было?
+    const renderPhrase = (number) => {
+
+    }
+
     return(
-        <h1>Users</h1>
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                users.map((item, idx) => {
+                    return(
+                        <>
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>
+                                    {
+                                        item.qualities.map((qualification) => {
+                                            return <span className={`bg-${qualification.color} m-1 p-1 text-white rounded`}>{qualification.name}</span>
+                                        })
+                                    }
+                                </td>
+                                <td>{item.profession.name}</td>
+                                <td>{item.completedMeetings}</td>
+                                <td>{item.rate}</td>
+                                <td><button onClick={() => setUsers(users.filter(value => value._id !== item._id))} className={'btn btn-danger'}>Удалить</button></td>
+                            </tr>
+                        </>
+                    )
+                })
+            }
+            </tbody>
+        </table>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Users from "./components/users";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
